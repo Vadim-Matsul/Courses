6ad6bef56d416858dd448416baacb7b1ea22c9b5
Vadim <Matsulmoscow@gmail.com>
2022-09-09 10:26:21 +0300

    Старт проекта

--- types/data.ts
+++ types/data.ts
@@ -1,13 +1,35 @@
-export type Post = {
-    userId: number,
-    id: number,
-    title: string,
-    body: string
+type Info = {
+  desc : string,
+  isActive : boolean
 }
 
-export type User = {
-    id: string,
-    name: string
+type Tags = {
+  name : string,
+  value : number
 }
 
-export type Posts = Post[]
+type User = {
+  userId : number,
+  id : number,
+  title : string
+  info : Info,
+  tags : Tags[]
+}
+
+let obj:User = {
+  userId: 1,
+    id: 1,
+      title: "delectus aut autem",
+        info: {
+    desc: "delectus aut autem",
+      isActive: true
+  },
+  tags: [
+    {
+      name: "my name",
+      value: 1000
+    }
+  ]
+}
+
+export {};
